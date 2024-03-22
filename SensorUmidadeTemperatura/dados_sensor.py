078350d7bb050994ac2a40e1f03ff12066b64e2d
joao-miziaras <joao.miziara@sptech.school>
2024-03-21 21:34:40 -0300

    update

--- SensorUmidadeTemperatura/dados_sensor.py
+++ SensorUmidadeTemperatura/dados_sensor.py
@@ -9,11 +9,13 @@ def get_temp():
 def get_umi():
     interval1 = (77, 80)  
     interval2 = (80, 88)  
+    interval3 = (88, 100)  
     
-    prob_interval1 = 0.2  
-    prob_interval2 = 0.3  
+    prob_interval1 = 0.3  
+    prob_interval2 = 0.4  
+    prob_interval3 = 0.1  
 
-    interval_escolhido = random.choices([interval1, interval2], weights=[prob_interval1, prob_interval2])[0]
+    interval_escolhido = random.choices([interval1, interval2, interval3], weights=[prob_interval1, prob_interval2, prob_interval3])[0]
 
     return round(random.uniform(*interval_escolhido), 2)
 def conectar_bd():
@@ -33,13 +35,24 @@ def insert_data(temperatura, umidade, horario):
     conexao.commit()
     conexao.close()
 
-while True:
-    temperatura = get_temp()
-    umidade = get_umi()
-    horario = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    print("Id do sensor: 1")
-    print(f"Percentual de umidade: {umidade}%")
-    print("Temperatura:", temperatura)
-    print("Horário da coleta:", horario)
-    insert_data(temperatura, umidade, horario)
-    time.sleep(5)
+contador_alerta = 0
+id_dado = 0
+try:
+    while True:
+        id_dado += 1
+        temperatura = get_temp()
+        umidade = get_umi()
+        horario = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        print("-" * 30)
+        print("Id do sensor: 1")
+        print("ID do dado:", id_dado)
+        print(f"Percentual de umidade: {umidade}%")
+        print("Temperatura:", temperatura)
+        print("Horário da coleta:", horario)
+        #insert_data(temperatura, umidade, horario)
+        if umidade > 88.0:
+            print("ALERTA: Condições climáticas desfavoráveis")
+            contador_alerta += 1
+        time.sleep(5)
+except KeyboardInterrupt:
+    print("Contador de alert de umidade alta:", contador_alerta)
